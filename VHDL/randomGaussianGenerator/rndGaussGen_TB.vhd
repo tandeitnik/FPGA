57b53b7dcf6fdaccdad190af04ce99eb4fa09967
tandeitnik <69469166+tandeitnik@users.noreply.github.com>
2024-02-17 12:03:43 -0300

    Update rndGaussGen_TB.vhd

--- VHDL/randomGaussianGenerator/rndGaussGen_TB.vhd
+++ VHDL/randomGaussianGenerator/rndGaussGen_TB.vhd
@@ -8,28 +8,32 @@ entity rndGaussGen_TB is
 end rndGaussGen_TB;
 
 architecture behav of rndGaussGen_TB is
-	signal CLK    : std_logic;
-    signal OUTPUT : std_logic_vector(13 downto 0);
+	signal clk_i    : std_logic;
+    signal rndNumb  : std_logic_vector(13 downto 0);
+	signal Y        : std_logic_vector(1 downto 0);
 begin
 	
     UUT : entity work.rndGaussGen
- port map(clk_i => CLK, output => OUTPUT);
+ port map(clk_i => clk_i, rndNumb => rndNumb, Y => Y);
     
     CLOCK_GEN : process
     file test_vector      : text open write_mode is "test.txt";
 	variable row          : line;
     begin
-        CLK <= '0';
+        clk_i <= '0';
         wait for 1 ns;
-        --write(row,CLK, right, 15);
-        --write(row,output, right, 15);
+        --write(row,clk_i, right, 20);
+        --write(row,rndNumb, right, 20);
+		--write(row,Y, right, 20);
         --writeline(test_vector,row);
-        CLK <= '1';
+        clk_i <= '1';
         wait for 1 ns;
-
-          write(row,CLK, right, 15);
-          write(row,output, right, 15);
-          writeline(test_vector,row);
+		if (Y = "01") then
+        --write(row,clk_i, right, 20);
+        write(row,rndNumb, right, 20);
+		--write(row,Y, right, 20);
+        writeline(test_vector,row);
+        end if;
 
     end process;
     
