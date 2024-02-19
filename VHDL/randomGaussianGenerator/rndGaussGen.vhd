ebde811d3e7f9a83a02d7ab32b1d08adfac36282
tandeitnik <69469166+tandeitnik@users.noreply.github.com>
2024-02-19 17:56:02 -0300

    Update rndGaussGen.vhd

--- VHDL/randomGaussianGenerator/rndGaussGen.vhd
+++ VHDL/randomGaussianGenerator/rndGaussGen.vhd
@@ -3,99 +3,66 @@
 	use IEEE.NUMERIC_STD.ALL;
 
 	-- entity
-	entity rndGaussGen is
-		port     (clk_i   : in  std_logic;
+	entity rndGaussGen2 is
+		port     (clk_i  : in  std_logic;
 				  rndNumb : out std_logic_vector(13 downto 0) );
-	end rndGaussGen;
+	end rndGaussGen2;
 
-	architecture behav of rndGaussGen is
-		
-		type memory is array (0 to 15) of signed(17 downto 0); --memory has to have 4 extra bits to accomodate the sum of 16 numbers without overflowing
-		type state_type is (ST0,ST1);
-		signal PS, NS : state_type;
-		signal uniformNumbers : memory; --matrix to store uniform distributed numbers
-		signal tmp : unsigned(13 downto 0);
-		
+	architecture behav of rndGaussGen2 is
+	type memory is array (0 to 15) of unsigned(13 downto 0); --memory has to have 4 extra bits to accomodate the sum of 16 numbers without overflowing
+	signal uniformNumbers : memory; --matrix to store uniform distributed numbers
 	begin
 
-		sync_proc: process(clk_i,NS)
+		process(clk_i)
 		variable CLR : integer := 1;
+		variable count : integer := 0;
 		begin
-			if (CLR = 1) then
-				PS <= ST0;
-				CLR := 0;
-			elsif (rising_edge(clk_i)) then
-				PS <= NS;
-			end if;
-		end process sync_proc;
-		
-		main: process(PS,clk_i)
-		variable count_1 : integer := 0;
-		variable count_2 : integer := 0;
-		variable sum     : signed(17 downto 0);
-		begin
-			case PS is
-				when ST0 => --build uniformNumbers matrix
-					
-					if (rising_edge(clk_i)) then
-						
-						if (count_1 = 0) then --place seed at fist row
-							tmp <= (0 => '1', others => '0');
-							uniformNumbers(count_1) <= resize(signed(std_logic_vector(tmp)),18); 
-							count_1 := count_1 + 1;
-							NS <= ST0; --continue in this state
-						elsif (count_1 < 16) then -- evaluate next rows
-							tmp <= shift_right(tmp , 1) OR shift_left( tmp XOR shift_right(tmp , 1) XOR shift_right(tmp , 2) XOR shift_right(tmp , 12)  , 13) ;
-							uniformNumbers(count_1) <= resize(signed(std_logic_vector(tmp)),18); 
-							count_1 := count_1 + 1;
-							NS <= ST0; --continue in this state
-						elsif (count_1 = 16) then -- matrix is done, advance to next state
-							NS <= ST1;--go to the next state
-						end if;
-						
-						rndNumb <= (others => '0'); --rndNumb zeros when building matrix
-						--rndNumb <= std_logic_vector(resize(tmp,18));
-					end if;
-							
-				when ST1 =>
-				
-					if (rising_edge(clk_i)) then
-					
-						sum := shift_right(  ( uniformNumbers(0)
-								 + uniformNumbers(1)
-								 + uniformNumbers(2)
-								 + uniformNumbers(3)
-								 + uniformNumbers(4)
-								 + uniformNumbers(5)
-								 + uniformNumbers(6)
-								 + uniformNumbers(7)
-								 + uniformNumbers(8)
-								 + uniformNumbers(9)
-								 + uniformNumbers(10)
-								 + uniformNumbers(11)
-								 + uniformNumbers(12)
-								 + uniformNumbers(13)
-								 + uniformNumbers(14)
-								 + uniformNumbers(15 )) , 4); --sum 16 entries and divide by 16 (which is equivalent to srl 4)
-								 
-						for i in 0 to 14 loop -- shift the registers up
-							uniformNumbers(i) <= uniformNumbers(i+1);
-						end loop;
-						
-						tmp <= shift_right(tmp , 1) OR shift_left( tmp XOR shift_right(tmp , 1) XOR shift_right(tmp , 2) XOR shift_right(tmp , 12)  , 13) ;
-						uniformNumbers(15) <= resize(signed(std_logic_vector(tmp)),18); 
-						rndNumb <= std_logic_vector(resize(sum,14));
-						
-					end if;
-					
-					NS <= ST1;
-					
-				when others =>
-				
-					NS <= ST0;
-					
-			end case;
+			if (rising_edge(clk_i)) then
 			
-		end process main;
-	
+				if (CLR = 1) then
+					if (count = 0) then
+						uniformNumbers(0) <= (0 => '1', others => '0');
+						count := count + 1;
+						rndNumb <= (others => '0');
+					elsif (count < 16) then
+						uniformNumbers(count) <= (uniformNumbers(count-1) srl 1) OR ((uniformNumbers(count-1) XOR (uniformNumbers(count-1) srl 1) XOR (uniformNumbers(count-1) srl 2) XOR (uniformNumbers(count-1) srl 12) ) sll 13) ;
+						rndNumb <= (others => '0');
+						count := count + 1;
+					elsif (count = 16) then
+						CLR := 0;
+						rndNumb <= (others => '0');
+					else
+						null;
+                    end if;
+				else
+					rndNumb <= std_logic_vector(resize(shift_right(
+												  resize(signed(std_logic_vector(uniformNumbers(0))),18) 
+												+ resize(signed(std_logic_vector(uniformNumbers(1))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(2))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(3))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(4))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(5))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(6))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(7))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(8))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(9))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(10))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(11))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(12))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(13))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(14))),18)
+												+ resize(signed(std_logic_vector(uniformNumbers(15))),18),4),14));
+					
+					for i in 0 to 14 loop
+						uniformNumbers(i) <= uniformNumbers(i+1);
+					end loop;
+					
+					uniformNumbers(15) <= (uniformNumbers(15) srl 1) OR ((uniformNumbers(15) XOR (uniformNumbers(15) srl 1) XOR (uniformNumbers(15) srl 2) XOR (uniformNumbers(15) srl 12) ) sll 13) ;
+					
+				end if;
+			end if;
+		
+		end process;
+
+		
 	end behav;
